43b031819376957328b97f3d3eeb8029903c1297
锐 <oncwnuLddSzv0V3IoVlpl083JCBk@git.weixin.qq.com>
2022-04-09 18:29:27 +0800

    vdoing

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,11 +4,11 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   lang: 'zh-CN',
-  theme: 'vdoing', // 使用npm包主题
-  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+  //theme: 'vdoing', // 使用npm包主题
+  theme: require.resolve('../../vdoing'), // 使用本地主题
 
   title: "修真门派掌门路",
-  description: 'GIS/RS 数据与经验资源导航',
+  description: "本命/灵根/同参的世界",
   // themeConfig: {
   //   sidebar: { mode: 'structuring', collapsable: false} //  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义
   // },
