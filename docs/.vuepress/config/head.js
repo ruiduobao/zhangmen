7b654c13f2546267d76a28134bc926148752a0bf
锐多宝的地理空间 <a2281138561@foxmail.com>
2022-05-10 10:42:05 +0800

    1

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -43,11 +43,11 @@ module.exports = [
   {},
   `
   var el = document.createElement("script");
-  el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?57b4417e5af27bb556862ddd1035af3b86d25aa934a0544184d9d963cc7f779465e0a2ada1d5e86b11e7de7c1a83287d04743a02fd1ee8dd8558a8cad50e91cb354f8c6f3f78e5fd97613c481f678e6d";
+  el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?63e938b7b2f130d6f9b60dbb31836b5912c7f83f73c02e831bae25f69978bf5431a338bfe8517054addc861a521fc04670aa30cdf5a37e663d5331479b5a12d275476d6d0d2e368be4dbc26e37139219";
   el.id = "ttzz";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(el, s);
-})();
+  })(window)
     `
 ],
 //360js提交收录
